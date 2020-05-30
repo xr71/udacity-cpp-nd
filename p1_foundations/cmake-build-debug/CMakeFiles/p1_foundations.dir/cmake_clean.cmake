78a15e278a33d5224f9e9535a59e88a5b04254e2
Xu Ren <musicalxu@hotmail.com>
2020-05-30 17:12:37 +0800

    p1 foundations init

---
+++ p1_foundations/cmake-build-debug/CMakeFiles/p1_foundations.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p1_foundations.dir/main.cpp.obj"
+  "p1_foundations.exe"
+  "p1_foundations.exe.manifest"
+  "p1_foundations.lib"
+  "p1_foundations.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p1_foundations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
