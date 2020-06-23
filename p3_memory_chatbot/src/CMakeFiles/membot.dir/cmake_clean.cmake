ce108ddbb7a647716571ea9084ce356ba8e86242
Xu Ren <musicalxu@hotmail.com>
2020-06-23 16:39:20 +0800

    proj init build

---
+++ p3_memory_chatbot/src/CMakeFiles/membot.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/membot.dir/chatbot.cpp.o"
+  "CMakeFiles/membot.dir/chatgui.cpp.o"
+  "CMakeFiles/membot.dir/chatlogic.cpp.o"
+  "CMakeFiles/membot.dir/graphedge.cpp.o"
+  "CMakeFiles/membot.dir/graphnode.cpp.o"
+  "membot.pdb"
+  "membot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/membot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
